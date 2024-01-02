d33aebea2888947fa68e43a6f8dea0cc2848ccc7
zimpson <zimpson@gmail.com>
2024-01-02 11:15:14 +0100

    could not fix master branch camera, stupid

--- Frontend/Assets/Scripts/Objects/Exit.cs
+++ Frontend/Assets/Scripts/Objects/Exit.cs
@@ -37,7 +37,6 @@ public class Exit : SludgeObject
     void SetActive(bool active)
     {
         SetAlpha(active ? 1.0f : 0.5f);
-        HighlightParticles.gameObject.SetActive(active);
         isActive = active;
     }
 
