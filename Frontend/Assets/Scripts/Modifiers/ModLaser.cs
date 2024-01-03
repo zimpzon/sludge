24980d351010c113ff3a005aec4c29559d90b418
zimpson <zimpson@gmail.com>
2024-01-03 16:49:15 +0100

    laser update

--- Frontend/Assets/Scripts/Modifiers/ModLaser.cs
+++ Frontend/Assets/Scripts/Modifiers/ModLaser.cs
@@ -1,31 +1,40 @@
 using Sludge.Modifiers;
 using Sludge.Utility;
 using UnityEngine;
-using static UnityEngine.EventSystems.EventTrigger;
 
 public class ModLaser : SludgeModifier
 {
+    public Transform BodyTrans;
+    public Transform LineTrans;
+    public float Quiver = 0.1f;
     LineRenderer line;
-    ParticleSystem particles;
-    ParticleSystem particlesFixed;
+    ParticleSystem particlesWorldSpace;
+    ParticleSystem particlesLocalSpace;
     Transform trans;
     ModTimeToggle timeToggle;
-    
+    Vector3 bodyBaseScale;
+    Vector3 lineBaseScale;
+
     private void Awake()
     {
         trans = transform;
+        bodyBaseScale = BodyTrans.localScale;
+        lineBaseScale = LineTrans.localScale;
         line = GetComponentInChildren<LineRenderer>();
         line.positionCount = 2;
-        particles = transform.Find("Particles").GetComponentInChildren<ParticleSystem>();
-        particlesFixed = transform.Find("ParticlesFixed").GetComponentInChildren<ParticleSystem>();
+        particlesWorldSpace = SludgeUtil.FindByName(transform, "ParticlesWorldSpace").GetComponentInChildren<ParticleSystem>();
+        particlesLocalSpace = SludgeUtil.FindByName(transform, "ParticlesLocalSpace").GetComponentInChildren<ParticleSystem>();
         timeToggle = GetComponent<ModTimeToggle>();
     }
 
     public override void Reset()
     {
+        BodyTrans.localScale = bodyBaseScale;
+        LineTrans.localScale = lineBaseScale;
+
         line.enabled = false;
-        particles.gameObject.SetActive(false);
-        particlesFixed.gameObject.SetActive(false);
+        particlesWorldSpace.gameObject.SetActive(false);
+        particlesLocalSpace.gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -39,17 +48,26 @@ public class ModLaser : SludgeModifier
     {
         if (timeToggle.Active && !timeToggle.IsOn())
         {
+            BodyTrans.localScale = bodyBaseScale;
+            LineTrans.localScale = lineBaseScale;
+
             line.enabled = false;
-            particles.gameObject.SetActive(false);
-            particlesFixed.gameObject.SetActive(false);
+            particlesWorldSpace.gameObject.SetActive(false);
+            particlesLocalSpace.gameObject.SetActive(false);
             return;
         }
+
+        // slight quivering when laser is on
+        Vector3 offset = new Vector3(Random.value * Quiver, Random.value * Quiver, 0);
+        BodyTrans.localScale = bodyBaseScale + offset;
+        //offset *= 0.25f;
+        //LineTrans.localScale = lineBaseScale + offset;
 
         if (!line.enabled)
         {
             line.enabled = true;
-            particles.gameObject.SetActive(true);
-            particlesFixed.gameObject.SetActive(true);
+            particlesWorldSpace.gameObject.SetActive(true);
+            particlesLocalSpace.gameObject.SetActive(true);
         }
 
         var direction = (line.transform.rotation * Vector2.right).normalized;
@@ -64,8 +82,8 @@ public class ModLaser : SludgeModifier
         line.SetPosition(0, Vector2.zero);
         line.SetPosition(1, new Vector2(distance, 0));
 
-        particles.transform.position = hit.point;
-        particlesFixed.transform.position = hit.point;
+        particlesWorldSpace.transform.position = hit.point;
+        particlesLocalSpace.transform.position = hit.point;
 
         // length of laser is determined, check if it hits something killable
         RaycastHit2D killableTarget = Physics2D.Raycast(trans.position, direction, distance, SludgeUtil.KillableLayerMask);
