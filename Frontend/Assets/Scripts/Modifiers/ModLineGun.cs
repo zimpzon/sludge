54f780c86ff925dead3e79d3949d6239dd3aed20
zimpson <zimpson@gmail.com>
2025-11-10 07:14:10 +0100

    fixed bullet at 0,0 bug

--- Frontend/Assets/Scripts/Modifiers/ModLineGun.cs
+++ Frontend/Assets/Scripts/Modifiers/ModLineGun.cs
@@ -40,6 +40,8 @@ public class ModLineGun : SludgeModifier
             var bullet = BulletManager.Instance.Get();
             if (bullet != null)
             {
+                bullet.transform.position = trans.position;
+
                 var look = SludgeUtil.LookAngle(trans.rotation.eulerAngles.z);
                 bullet.DX = SludgeUtil.Stabilize(look.x * BulletSpeed);
                 bullet.DY = SludgeUtil.Stabilize(look.y * BulletSpeed);
