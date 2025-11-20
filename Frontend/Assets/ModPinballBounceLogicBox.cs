d8d325eeca0ecbb564b94b8d3bdfc8b9a896f51f
zimpson <zimpson@gmail.com>
2025-11-20 06:49:38 +0100

    backup

--- Frontend/Assets/ModPinballBounceLogicBox.cs
+++ Frontend/Assets/ModPinballBounceLogicBox.cs
@@ -19,7 +19,8 @@ public class ModPinballBounceLogicBox : SludgeModifier
         var entity = SludgeUtil.GetEntityType(collision.gameObject);
         if (entity == EntityType.Player)
         {
-            var direction = Vector2.up;
+            var normal = collision.contacts[0].normal;
+            var direction = Vector2.Dot(normal, Vector2.up) > 0.7f ? Vector2.down : Vector2.up;
             Player.I.AddForceDirection(direction);
             bodyTrans.DOKill(complete: true);
             bodyTrans.DOPunchScale(Vector3.one * 0.2f, 0.2f);
