321a986fa0c1bf984773250d37228bd5823a91ac
zimpson <zimpson@gmail.com>
2025-11-20 16:06:20 +0100

    backup

--- Frontend/Assets/OneWay.cs
+++ Frontend/Assets/OneWay.cs
@@ -1,7 +1,24 @@
 using Sludge.SludgeObjects;
 using Sludge.Utility;
+using UnityEngine;
 
 public class OneWay : SludgeObject
 {
     public override EntityType EntityType => EntityType.Enemy;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1f, 0.8f, 0f, 0.7f);
+
+        Vector3 position = transform.position;
+        Vector3 scale = transform.localScale;
+
+        float height = 0.5f;
+        float width = scale.x;
+
+        Gizmos.DrawWireCube(position, new Vector3(width, height, 1f));
+
+        Gizmos.color = new Color(1f, 0.8f, 0f, 0.3f);
+        Gizmos.DrawCube(position, new Vector3(width, height, 1f));
+    }
 }
